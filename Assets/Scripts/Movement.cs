3d34985aa3f246a052aba8e53f7874dda41ebbf0
Colby Williams <mehthiswilldo@gmail.com>
2017-09-23 18:36:34 -0400

    I forget what I did

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,29 +14,37 @@ public class Movement : MonoBehaviour {
 
     void Start()
     {
+        //You don't start jumping
         isJumping = false;
     }
 
     void FixedUpdate()
     {
+        //Sets base x and y movement
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = GetComponent<Rigidbody2D>().velocity.y;
 
+        //Makes the velocity vector that we'll use to edit the player later
         Vector3 velocity = new Vector3(moveHorizontal * sideSpeed, moveVertical, 0.0f);
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        //If you press the up key to jump...
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            if (isJumping)
+            //Don't jump if you're already jumping
+            if (!isJumping)
             {
-                return;
+                //Ok you can jump
+                Debug.LogError("I tried");
+                velocity.y = jumpSpeed;
+                isJumping = true;
             }
-
-            velocity.y = jumpSpeed;
-            isJumping = true;
         }
 
+        //Setting player velocity to velocity
+        //GetComponent<Rigidbody2D>().AddForce(velocity);
         GetComponent<Rigidbody2D>().velocity = velocity;
 
+        //Flips the sprite as needed
         if (velocity.x > 0.1)
         {
             GetComponent<SpriteRenderer>().flipX = false;
@@ -45,7 +53,7 @@ public class Movement : MonoBehaviour {
             GetComponent<SpriteRenderer>().flipX = true;
         }
 
-
+        //Lets the animator know if the player is moving
         GetComponent<Animator>().SetFloat("Speed", Mathf.Abs(velocity.x));
 
         
